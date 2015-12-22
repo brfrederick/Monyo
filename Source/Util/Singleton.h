5c1f1fec07f4ec8cc0d20f669f2fcb48208fb28f
brax <baf9201@rit.edu>
2015-12-22 08:52:56 -0500

    Added a basic singleton class

--- Source/Util/Singleton.h
+++ Source/Util/Singleton.h
@@ -1,1 +1,23 @@
 #pragma once
+
+template <typename T>
+class Singleton 
+{
+
+public:
+	static T& Get() 
+	{
+		static T instance;
+		return instance;
+	}
+
+private:
+	Singleton<T>() {};
+	Singleton<T>(Singleton<T> const&);
+	void operator=(Singleton<T> const&) = {};
+
+public:
+	Singleton<T>(Singleton<T> const&) = delete;
+	void operator=(Singleton<T> const&) = delete;
+
+};

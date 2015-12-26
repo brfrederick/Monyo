086bccea4cdc1fef812cf7ce6c5719b03c62843f
brax <baf9201@rit.edu>
2015-12-25 20:58:22 -0500

    Stubbed out Model object

--- Source/Assets/Model.h
+++ Source/Assets/Model.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <d3d11.h>
+
+class Model 
+{
+public:
+	Model() {};
+	~Model() {};
+
+	ID3D11Buffer** GetVertexBuffer() { return &m_vertexBuffer; }
+	ID3D11Buffer* GetIndexBuffer() { return m_indexBuffer; }
+	int GetIndexCount() { return m_indexCount; }
+
+protected:
+	int m_indexCount;
+	ID3D11Buffer* m_vertexBuffer;
+	ID3D11Buffer* m_indexBuffer;
+
+};
